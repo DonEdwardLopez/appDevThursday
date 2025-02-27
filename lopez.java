f4c7cba5b0bdf4a39bbeca62d46db12bb0eadc33
DonEdwardLopez <donedwardlopezrider@gmail.com>
2025-02-27 20:16:39 +0800

    Create lopez.java

---
+++ lopez.java
@@ -0,0 +1,20 @@
+week4.java
+
+public class week4 {
+    public static void main(String[] args) {
+        System.out.println("Collaborative Java Project\n");
+       
+        // Call each student's method
+        lopez1.display();
+       
+        // More students will add their method calls here...
+    }
+}
+
+lopez1.java
+
+public class student1 {
+    public static void display() {
+        System.out.println("Hello! This is lopez1 contributing to the project.");
+    }
+}
